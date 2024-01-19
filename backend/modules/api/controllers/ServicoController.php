aebac82aebf61e9d08d5b2ca482d508ddc28ff9d
Diogo Gonçalves <2211879@my.ipleiria.pt>
2024-01-19 03:12:18 +0000

    Update ServicoController.php

--- backend/modules/api/controllers/ServicoController.php
+++ backend/modules/api/controllers/ServicoController.php
@@ -9,6 +9,29 @@ class ServicoController extends ActiveController
 {
     public $modelClass = 'common\models\Servicos';
 
+
+    //Permite ir buscar o valor da percentagem do iva_id associado
+    public function actionGetIva()
+    {
+        $model = new $this->modelClass;
+
+        $servicos = $model::find()
+            ->select([
+                'servicos.id',
+                'servicos.referencia',
+                'servicos.nome',
+                'servicos.descricao',
+                'servicos.preco',
+                'servicos.ativo',
+                'servicos.iva_id',
+                '(SELECT percentagem FROM ivas WHERE id = servicos.iva_id) AS ivaspercentagem'
+            ])
+            ->asArray()
+            ->all();
+
+        return $servicos;
+    }
+
     public function actionServicospeladescricao($descricao)
     {
         $servicomodel = new $this->modelClass;
